a805d4d292c11398ea6dc1b1d8dfbc192faff4e1
Thanzeela K A <thanzeela.ka@experionglobal.local>
2022-11-19 17:07:20 +0530

    first commit

---
+++ data/customers.js
@@ -0,0 +1,13 @@
+const db = require('./db');
+function getAll(callback){
+const sql = "SELECT id,name,user_name,email,password,mobile_number,address,dob FROM passenger";
+db.executequery(sql, [], callback);
+}
+function addOne(name,user_name,email,password,mobile_number,address,dob ,callback){
+    const sql ="INSERT INTO  passenger(name,user_name,email,password,mobile_number,address,dob)VALUES(?,?,?,?,?,?,?)";
+    const params = [name,user_name, email,password,mobile_number,address,dob];
+    console.log(params)
+db.executequery(sql, params, callback);
+}
+module.exports.getAll= getAll;
+module.exports.addOne = addOne;
